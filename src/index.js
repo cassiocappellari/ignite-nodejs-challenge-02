fa5f12ff7f4050611b797b73b1ae3ce2670615ed
cassiocappellari <cassiocappellari@gmail.com>
2021-03-17 08:27:00 -0300

    feat: update checksExistsUserAccount, checksCreateTodosUserAvailability, checksTodoExists and findUserById middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,80 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    (user) => user.username === username
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" })
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const userTodos = user.todos.length;
+  const userPlan = user.pro;
+
+  if (userTodos >= 10 && userPlan === false) {
+    return response.status(403).json({ error: "you reach the limit of 10 todos in the free plan" })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.username === username
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" })
+  };
+
+  const idLength = id.length;
+
+  if (idLength !== 36) {
+    return response.status(400).json({ error: "invalid id" })
+  };
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+
+  if (!todo) {
+    return response.status(404).json({ error: "todo not found" })
+  };
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.id === id
+  );
+
+  if (!user) {
+    return response.status(404).json({ error: "id not found" })
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
